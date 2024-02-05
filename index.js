673423b88e41c6d7c60851eeff118f195ab575ad
Fernando <GutierrezT.LFernando@gmail.com>
2024-02-05 00:44:45 -0500

    base de datos para modelos

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const {
   encriptarContrasena,
   DetectarPasswords,
 } = require("./funciones/Encriptar");
+const {generarCodigoAleatorio} = require("./funciones/CodAleatorio");
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const multer = require('multer');
+const multer = require("multer");
 const app = express();
 const port = 5000;
 
@@ -93,17 +94,17 @@ app.post("/modUser", (req, res) => {
     Contacto,
     User,
     Password,
-    idUser
+    idUser,
   } = nuevosDatos;
   const setValues = {
-    DocumentoIdentidad:DocumentoIdentidad,
+    DocumentoIdentidad: DocumentoIdentidad,
     Nombres: Nombres,
     Apellidos: Apellidos,
     Cargo: Cargo,
     Informacion_Contacto: Contacto,
   };
-  const consulta = 'UPDATE Empleado SET ? WHERE ID_Empleado = ?';
-  db.query(consulta, [setValues, idUser] , (err, result) => {
+  const consulta = "UPDATE Empleado SET ? WHERE ID_Empleado = ?";
+  db.query(consulta, [setValues, idUser], (err, result) => {
     if (err) {
       res.status(500).send("Error del servidor");
     } else {
@@ -111,7 +112,6 @@ app.post("/modUser", (req, res) => {
     }
   });
 });
-
 
 //Validar la contraseña al iniciar sesion
 app.post("/validateUser", (req, res) => {
@@ -155,46 +155,74 @@ app.post("/dataTrabajador", (req, res) => {
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 //GESTION DE IMAGENES
-app.post('/newImage', upload.single('image'), (req, res) => {
-  const { nombre } = req.body;
+app.post("/newImage", upload.single("image"), (req, res) => {
+  const { nombre, tipo } = req.body;
   const imageBuffer = req.file.buffer;
 
   // Aquí deberías insertar la imagen y otros datos en tu base de datos
-  const sql = 'INSERT INTO Imagenes (Nombre, Image) VALUES (?, ?)';
-  db.query(sql, [nombre, imageBuffer], (error, results, fields) => {
+  const sql = "INSERT INTO Imagenes (Nombre, Image, Tipo) VALUES (?, ?, ?)";
+  db.query(sql, [nombre, imageBuffer, tipo], (error, results, fields) => {
     if (error) {
-      console.error('Error al insertar en la base de datos:', error);
-      return res.status(500).json({ error: 'Error al insertar en la base de datos' });
+      console.error("Error al insertar en la base de datos:", error);
+      return res
+        .status(500)
+        .json({ error: "Error al insertar en la base de datos" });
     }
 
-    res.json({ success: true, message: 'Imagen subida correctamente' });
+    res.json({ success: true, message: "Imagen subida correctamente" });
   });
 });
 
-app.get('/getIdNamesImage', (req, res) => {
-  const sql = 'SELECT ID_Image, Nombre FROM Imagenes';
+app.get("/getIdNamesImage", (req, res) => {
+  const sql = "SELECT ID_Image, Nombre FROM Imagenes";
   db.query(sql, (error, results, fields) => {
     if (error) {
-      console.error('Error al obtener IDs y nombres de la base de datos:', error);
-      return res.status(500).json({ error: 'Error al obtener IDs y nombres de la base de datos' });
+      console.error(
+        "Error al obtener IDs y nombres de la base de datos:",
+        error
+      );
+      return res
+        .status(500)
+        .json({ error: "Error al obtener IDs y nombres de la base de datos" });
     }
 
     res.json(results);
   });
 });
 
-app.post('/getImageById', (req, res) => {
+app.post("/getInfoImageByType", (req, res) => {
+  const { tipo } = req.body;
+  const sql = "SELECT ID_Image, Nombre FROM Imagenes WHERE Tipo = ?";
+
+  db.query(sql, [tipo], (error, results, fields) => {
+    if (error) {
+      console.error(
+        "Error al obtener las imágenes de la base de datos:",
+        error
+      );
+      return res
+        .status(500)
+        .json({ error: "Error al obtener las imágenes de la base de datos" });
+    }
+
+    res.status(200).json(results);
+  });
+});
+
+app.post("/getImageById", (req, res) => {
   const { id } = req.body;
 
-  const sql = 'SELECT Image FROM Imagenes WHERE ID_Image = ?';
+  const sql = "SELECT Image FROM Imagenes WHERE ID_Image = ?";
   db.query(sql, [id], (error, results, fields) => {
     if (error) {
-      console.error('Error al obtener la imagen de la base de datos:', error);
-      return res.status(500).json({ error: 'Error al obtener la imagen de la base de datos' });
+      console.error("Error al obtener la imagen de la base de datos:", error);
+      return res
+        .status(500)
+        .json({ error: "Error al obtener la imagen de la base de datos" });
     }
 
     if (results.length === 0) {
-      return res.status(404).json({ error: 'Imagen no encontrada' });
+      return res.status(404).json({ error: "Imagen no encontrada" });
     }
 
     const { Image } = results[0];
@@ -202,36 +230,274 @@ app.post('/getImageById', (req, res) => {
   });
 });
 
-
-// COPNTROL DE PROVEEDORES 
-app.post('/newProveedor', async (req, res) => {
-
+// COPNTROL DE PROVEEDORES
+app.post("/newProveedor", async (req, res) => {
   const { nombre, informacionContacto, imagenAsociada } = req.body;
 
-  const sql = 'INSERT INTO Proveedor (Nombre, Informacion_Contacto, Imagen_Asociada) VALUES (?, ?, ?)';
+  const sql =
+    "INSERT INTO Proveedor (Nombre, Informacion_Contacto, Imagen_Asociada) VALUES (?, ?, ?)";
 
-  db.query(sql, [nombre, informacionContacto, imagenAsociada], (error, results, fields) => {
+  db.query(
+    sql,
+    [nombre, informacionContacto, imagenAsociada],
+    (error, results, fields) => {
+      if (error) {
+        console.error("Error al insertar en la base de datos:", error);
+        return res
+          .status(500)
+          .json({ error: "Error al insertar en la base de datos" });
+      }
+      const nuevoProveedorId = results.insertId;
+      res.status(200).json({ message: "Proveedor agregado con éxito" });
+    }
+  );
+});
+
+app.get("/getProveedores", (req, res) => {
+  const sql = "SELECT * FROM Proveedor";
+  db.query(sql, (error, results, fields) => {
     if (error) {
-      console.error('Error al insertar en la base de datos:', error);
-      return res.status(500).json({ error: 'Error al insertar en la base de datos' });
+      console.error("Error al obtener productos de la base de datos:", error);
+      return res
+        .status(500)
+        .json({ error: "Error al obtener productos de la base de datos" });
     }
 
-    res.status(200).json({ message: 'Proveedor agregado con éxito' });
+    res.json(results);
   });
-  
+});
+
+//CONTROL DE ALMACEN
+
+app.get("/getAlmacenes", (req, res) => {
+  const sql = "SELECT * FROM Almacen";
+  db.query(sql, (error, results, fields) => {
+    if (error) {
+      console.error(
+        "Error al obtener IDs y nombres de la base de datos:",
+        error
+      );
+      return res
+        .status(500)
+        .json({ error: "Error al obtener IDs y nombres de la base de datos" });
+    }
+
+    res.json(results);
+  });
+});
+
+app.post("/newAlmacen", async (req, res) => {
+  const {
+    Referencia,
+    Pais,
+    Departamento,
+    Provincia,
+    Distrito,
+    Direccion,
+    Capacidad,
+    Capacidad_Utilizada,
+    Imagen_Referencial,
+  } = req.body;
+  const codAl=generarCodigoAleatorio();
+  const sql =
+    "INSERT INTO Almacen (Referencia, Pais, Departamento, Provincia, Distrito, Direccion, Capacidad, Capacidad_Utilizada, Imagen_Referencial, AccesKey) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  db.query(
+    sql,
+    [
+      Referencia,
+      Pais,
+      Departamento,
+      Provincia,
+      Distrito,
+      Direccion,
+      Capacidad,
+      Capacidad_Utilizada,
+      Imagen_Referencial,
+      codAl
+    ],
+    (error, results, fields) => {
+      if (error) {
+        console.error("Error al insertar en la base de datos:", error);
+        return res
+          .status(500)
+          .json({ error: "Error al insertar en la base de datos" });
+      }
+
+      const nuevoAlmacenId = results.insertId; // Obtener el ID del nuevo almacén
+
+      res
+        .status(200)
+        .json({ message: "Almacén agregado con éxito", id: nuevoAlmacenId });
+    }
+  );
+});
+
+app.post("/editAlmacen", async (req, res) => {
+  const {
+    ID_Almacen,
+    Referencia,
+    Pais,
+    Departamento,
+    Provincia,
+    Distrito,
+    Direccion,
+    Capacidad,
+    Capacidad_Utilizada,
+    Imagen_Referencial,
+  } = req.body;
+  const sql =
+    "UPDATE Almacen SET Referencia=?, Pais=?, Departamento=?, Provincia=?, Distrito=?, Direccion=?, Capacidad=?, Capacidad_Utilizada=?, Imagen_Referencial=? WHERE ID_Almacen=?";
+
+  db.query(
+    sql,
+    [
+      Referencia,
+      Pais,
+      Departamento,
+      Provincia,
+      Distrito,
+      Direccion,
+      Capacidad,
+      Capacidad_Utilizada,
+      Imagen_Referencial,
+      ID_Almacen,
+    ],
+    (error, results, fields) => {
+      if (error) {
+        console.error("Error al actualizar en la base de datos:", error);
+        return res
+          .status(500)
+          .json({ error: "Error al actualizar en la base de datos" });
+      }
+
+      res.status(200).json({ message: "Almacén actualizado con éxito" });
+    }
+  );
 });
 
 
+app.post("/newEmpleadoAlmacen", async (req, res) => {
+  const { ID_Empleado, ID_Almacen } = req.body;
 
+  const sql =
+    "INSERT INTO Empleado_Almacen (ID_Empleado, ID_Almacen) VALUES (?, ?)";
 
+  db.query(sql, [ID_Empleado, ID_Almacen], (error, results, fields) => {
+    if (error) {
+      console.error("Error al insertar en la base de datos:", error);
+      return res
+        .status(500)
+        .json({ error: "Error al insertar en la base de datos" });
+    }
 
+    const nuevoEmpleadoAlmacenId = results.insertId; // Obtener el ID del nuevo registro
 
+    res
+      .status(200)
+      .json({
+        message: "Empleado_Almacen agregado con éxito",
+        id: nuevoEmpleadoAlmacenId,
+      });
+  });
+});
 
+app.get("/getAlmacenesEmpleado", (req, res) => {
+  const sql = "SELECT * FROM Empleado_Almacen";
+  db.query(sql, (error, results, fields) => {
+    if (error) {
+      console.error(
+        "Error al obtener empleados almacen:",
+        error
+      );
+      return res
+        .status(500)
+        .json({ error: "Error al obtener empleados almacen" });
+    }
 
+    res.json(results);
+  });
+});
 
+//----------------------------CONTROL MODELOS-----------------------//
+app.get("/getModelos", (req, res) => {
+  const sql = "SELECT * FROM Producto";
+  db.query(sql, (error, results, fields) => {
+    if (error) {
+      console.error("Error al obtener productos de la base de datos:", error);
+      return res
+        .status(500)
+        .json({ error: "Error al obtener productos de la base de datos" });
+    }
 
+    res.json(results);
+  });
+});
 
+app.post("/newModelo", async (req, res) => {
+  const {
+    ID_Proveedor,
+    Nombre,
+    Descripcion,
+    Material,
+    Tipo,
+    Imagen_Asociada,
+  } = req.body;
 
+  const sql =
+    "INSERT INTO Producto (ID_Proveedor, Nombre, Descripcion, Material, Tipo, Imagen_Asociada) VALUES (?, ?, ?, ?, ?, ?)";
+
+  db.query(
+    sql,
+    [ID_Proveedor, Nombre, Descripcion, Material, Tipo, Imagen_Asociada],
+    (error, results, fields) => {
+      if (error) {
+        console.error("Error al insertar producto en la base de datos:", error);
+        return res
+          .status(500)
+          .json({ error: "Error al insertar producto en la base de datos" });
+      }
+
+      const nuevoProductoId = results.insertId; // Obtener el ID del nuevo producto
+
+      res
+        .status(200)
+        .json({ message: "Producto agregado con éxito", id: nuevoProductoId });
+    }
+  );
+});
+
+app.post("/editModelo", async (req, res) => {
+  const {
+    ID_Producto,
+    ID_Proveedor,
+    Nombre,
+    Descripcion,
+    Material,
+    Tipo,
+    Imagen_Asociada,
+  } = req.body;
+
+  const sql =
+    "UPDATE Producto SET ID_Proveedor=?, Nombre=?, Descripcion=?, Material=?, Tipo=?, Imagen_Asociada=? WHERE ID_Producto=?";
+
+  db.query(
+    sql,
+    [ID_Proveedor, Nombre, Descripcion, Material, Tipo, Imagen_Asociada, ID_Producto],
+    (error, results, fields) => {
+      if (error) {
+        console.error("Error al actualizar producto en la base de datos:", error);
+        return res
+          .status(500)
+          .json({ error: "Error al actualizar producto en la base de datos" });
+      }
+
+      res.status(200).json({ message: "Producto actualizado con éxito" });
+    }
+  );
+});
+
+//-----------------------------SUBIR AL SERIDOR-------------------------//
 app.listen(port, () => {
   console.log(`Servidor backend en ejecución en http://localhost:${port}`);
 });
